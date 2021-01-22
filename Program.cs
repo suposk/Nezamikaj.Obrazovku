60c03844aba8742ccdea1b177f4757d616da2b35
suposk <jan.supolik@gmail.com>
2021-01-22 10:35:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nezamikaj.Obrazovku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stop = false;
+            Timer timer = null;
+
+            if (args != null && args.Count() > 0)
+            {
+                var s = args.FirstOrDefault();
+                if (int.TryParse(s, out int sec))
+                {
+                    Console.WriteLine($"Set timer to close app in {sec}");
+                    var ts = new TimeSpan(0, 0, 0, sec);
+
+                    timer = new Timer((a) => 
+                    {
+                        timer.Dispose();
+                        Environment.Exit(0);
+                    }, null, ts, ts);
+                }
+            }
+
+            UInt32 postX = 20;
+            UInt32 postY = 20;
+            UInt32 posMax = 500;
+            Console.WriteLine("To Stop Press Escape");
+            while (!stop)
+            {
+                for(UInt32 x = 0; x <= posMax; x++ )
+                {
+
+                    postX = x;
+                    Random rand = new Random();
+                    var del = rand.Next(2, 20);
+                    postY = (UInt32)del;
+                    Thread.Sleep(del * 1000);
+
+                    Win32.mouse_event((int)(Win32.MouseEventFlags.MOVE), postX, postY, 0, 0);
+                    Win32.mouse_event((int)(Win32.MouseEventFlags.LEFTDOWN), postX, postY, 0, 0);
+
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+                        switch (key.Key)
+                        {
+                            case ConsoleKey.Q:
+                            case ConsoleKey.Escape:
+                                {
+                                    Console.WriteLine("pressed key to stop");
+                                    timer?.Dispose();
+                                    stop = true;
+                                    break;
+                                }
+                            default:
+                                break;
+                        }
+                    }
+                    if (stop)
+                        break;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Win32.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nezamikaj.Obrazovku
+{
+    public class Win32
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+    }
+}
